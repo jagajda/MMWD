329ed308686e4e4eafe95e1a6382076a35454a35
jagajda <kuba.gajda123@gmail.com>
2018-12-16 13:51:22 +0100

    update population

--- pkg/Population.py
+++ pkg/Population.py
@@ -15,17 +15,18 @@ class Population:
         self.deathNum = deathNum
         self.bestFitVect = bestFitVect
 
-    def printTarget(self):
-        pyplot.plot(self.populationID, self.bestFitVect(0), self.populationID, self.bestFitVect(1), self.populationID, self.bestFitVect(2))
+    def plotTarget(self):
+        pyplot.plot(self.populationID, self.bestFitVect[0], self.populationID, self.bestFitVect[1], self.populationID, self.bestFitVect[2])
 
-    def printAverage(self):
-        avg = np.average(self.bestFitVect)
+    def plotAverage(self):
+        vec = [Element.getvalue(i) for i in self.specimenList]
+        avg = np.average(vec)
         pyplot.plot(self.populationID, avg)
 
-    def printStdDev(self):
+    def plotStdDev(self):
         vec = [Element.getvalue(i) for i in self.specimenList]
         stddev = np.std(vec)
-        pyplot.plot(self.populationID, stddev)
+        pyplot.plot(self.populationID, stddev,)
 
 
 def generatepopulation(numspecimen, storagelist, orderlist):
@@ -37,8 +38,8 @@ def generatepopulation(numspecimen, storagelist, orderlist):
 
 
 #testing pyplot
-#x = [1, 2, 3]
-#y1 = [i * 2 for i in x]
-#y2 = [j*j for j in x]
-#pyplot.plot(x, y1, x, y2)
-#pyplot.show()
+x = [1, 2, 3]
+y1 = [i * 2 for i in x]
+y2 = [j*j for j in x]
+s = pyplot.plot(x, y1, x, y2)
+pyplot.show(s)
