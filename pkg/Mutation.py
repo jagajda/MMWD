b4f3dd965382c347b7f63b7cfa749494d8c99cfa
lukaszex <lukaszex1997@gmail.com>
2018-12-22 19:51:23 +0100

    Update Mutation.py

--- pkg/Mutation.py
+++ pkg/Mutation.py
@@ -1,11 +1,15 @@
-def mutation (specimensList, mutType):
-    numberOfElements = len (specimensList [0])
-    for i in range (len (specimensList)):
+import random
+def mutation (specimenList, mutType):
+    #numberOfElements = len (specimensList [0])
+    for i in specimenList:
+        intList = i.intList
+        numberOfElements = len (intList)
         mutationPoint = random.randint (0, numberOfElements - 1)
         if mutType == 0:
-            specimensList [i][mutationPoint] = specimensList [i][mutationPiont] + random.randint (-1, 1)
+            intList[mutationPoint] = intList[mutationPoint] + random.randint (-1, 1)
         elif mutType == 1:
-            specimensList [i][mutationPoint] = specimensList [i][mutationPiont] + random.randint (-3, 3)
+            intList[mutationPoint] = intList[mutationPoint] + random.randint (-3, 3)
         elif mutType == 2:
-            specimensList [i][mutationPoint] = specimensList [i][mutationPiont] + random.randint (-5, 5)
-    return specimensList
+            intList[mutationPoint] = intList[mutationPoint] + random.randint (-5, 5)
+    return specimenList
+
