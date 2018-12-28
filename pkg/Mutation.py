471a2ebbc06f57c0ee77a4785557cb23214dae61
lukaszex <lukaszex1997@gmail.com>
2018-12-28 20:42:26 +0100

    mutation - random choice from storage

--- pkg/Mutation.py
+++ pkg/Mutation.py
@@ -1,15 +1,15 @@
 import random
-def mutation (specimenList, mutType):
-    #numberOfElements = len (specimensList [0])
+def mutation (specimenList, mutType, Storage):
+    numberOfElements = len (Storage.storageElements)
     for i in specimenList:
         intList = i.intList
-        numberOfElements = len (intList)
-        mutationPoint = random.randint (0, numberOfElements - 1)
+        #numberOfElements = len (intList)
+        mutationPoint = random.randint (0, len(intList) - 1)
         if mutType == 0:
-            intList[mutationPoint] = intList[mutationPoint] + random.randint (-1, 1)
+            intList[mutationPoint] = random.randint (0, round((numberOfElements - 1)/2))
         elif mutType == 1:
-            intList[mutationPoint] = intList[mutationPoint] + random.randint (-3, 3)
-        elif mutType == 2:
-            intList[mutationPoint] = intList[mutationPoint] + random.randint (-5, 5)
+            intList[mutationPoint] = random.randint (round((numberOfElements - 1)/2), numberOfElements - 1)
+        #elif mutType == 2:
+          #  intList[mutationPoint] = intList[mutationPoint] + random.randint (-5, 5)
     return specimenList
 
